f3650e6939488b1752eef2e9c687a50d3dca9787
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-01 21:52:49 -0800

    Update assignment4.js

--- assignment4.js
+++ assignment4.js
@@ -9,36 +9,45 @@ setup();
 strokeWeight(5);
 stroke = 50;
 
-line (120, 0, 120, 340);
-line (280, 0, 280, 340);
+var a = random(width)
+var b = random(height)
+var c = 340 - a - b
+
+
 fill(255, 0, 0); //red
-rect (50, 50, random(width), random(height));
-line (0, 30, 340, 30);
-line (0, 280, 340, 280);
-rect (120, 160, random(width), random(height));
-rect (160, 330, random(width), random(height));
-line (0, 120, 340, 120);
-line (160, 0, 160, 340);
-line (240, 0, 240, 340);
-
+rect (0, a, b, c);
+rect (b, a, b, c);
+fill (255); //white
 fill(255, 204, 0); //yellow
-rect (30, 330, random(width), random(height));
-rect (240, 120, random(width), random(height));
-line (50, 0, 50, 340);
-line (330, 0, 330, 340);
-rect (330, 30, random(width), random(height));
-
-line (0, 300, 340, 300);
-line (0, 330, 340, 330);
-line (20, 0, 20, 340);
-
+rect (c, c, a, b);
 fill(0, 80, 220); //blue
-rect (random(width), random(height), random(width), random(height));
-rect (random(width), random(height), random(width), random(height));
-rect (random(width), random(height), random(width), random(height));
+rect (c, c, b, a);
+fill(255, 204, 0); //yellow
+rect (a, a, b, c);
+fill(255, 0, 0); //red
+rect (a, b, b, c);
+rect (b, b, a, c);
+fill (255); //white
+rect (b, b, c, a);
+fill(0, 80, 220); //blue
+rect (a, b, b, b);
+rect (a, c, b, c);
+fill(255, 0, 0); //red
+rect (c, b, a, c);
+fill (255); //white
+rect (b, b, a, b);
+rect (a, c, c, c);
+fill(0, 80, 220); //blue
+rect (c, b, b, c);
+fill(255, 204, 0); //yellow
+rect (c, b, a, b);
+fill (255); //white
+rect (a, c, b, c);
+fill(255, 0, 0); //red
+rect (c, b, a, c);
 
 //frame
-strokeWeight(20);
+strokeWeight(15);
 stroke = 50;
 line (340, 340, 340, 0);
 line (0, 340, 340, 340);
