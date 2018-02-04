81fee401024f250d0dd455ce87806ae06ed80f45
TiaHuang <35742567+TiaHuang@users.noreply.github.com>
2018-02-04 15:54:05 -0800

    Update assignment2.js

--- assignment2.js
+++ assignment2.js
@@ -23,6 +23,6 @@ function setup (){
 var line2 = 0;
 while (line2 < 10) {
   stroke(x1+x1)
-  line(random(width), random(height), x1+x1, 0);
+  line(random(width), random(height), random(x1, x1+x1), random(0, x1));
   line2 = line2 + 1;
 }
